509d3584696da213ada89c95ab21427948bde92d
Rui Wu <wurui.dev@gmail.com>
2024-05-24 10:12:22 +0800

    fix: lint

--- src/types.ts
+++ src/types.ts
@@ -3,13 +3,13 @@ import type { builtInPlatforms } from './_constant'
 export type BuiltInPlatForm = typeof builtInPlatforms[number]
 
 export type UtsPlatform = BuiltInPlatForm
-| 'app-android'
-| 'app-ios'
-| 'web'
+  | 'app-android'
+  | 'app-ios'
+  | 'web'
 export type AppPlatform = 'android'
-| 'ios'
-| undefined
+  | 'ios'
+  | undefined
 export type SubPlatform =
-   'quickapp-webview-huawei'
-   | 'quickapp-webview-union'
-   | undefined
+  'quickapp-webview-huawei'
+  | 'quickapp-webview-union'
+  | undefined
